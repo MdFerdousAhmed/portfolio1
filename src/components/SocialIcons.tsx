afc2b549cc98b3f7e4faf61b9e381e84fb287dae
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-07-24 23:27:02 +0600

    change twitter icon

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -53,14 +53,10 @@ export function TwitterIcon({ size = 24, strokeWidth = 2, ...props }: IconProps)
       width={size}
       height={size}
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={strokeWidth}
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
       {...props}
     >
-      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+      <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.64 7.584H.47l8.6-9.83L0 1.154h7.594l5.243 6.932L18.901 1.153Zm-1.29 19.493h2.04L6.486 3.24H4.298L17.61 20.646Z" />
     </svg>
   );
 }
